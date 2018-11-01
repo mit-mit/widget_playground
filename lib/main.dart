beb59e8d93ed4e8130c2d3abc9ba321f21ef8ac2
Michael Thomsen <mit@google.com>
2018-11-01 15:11:05 +0100

    First demo

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Widget Playground',
+      title: 'Material Widget Playground',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Widget Playground'),
+      home: MyHomePage(title: 'Material Widget Playground'),
     );
   }
 }
@@ -24,13 +24,20 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+enum SupportedWidgets { FlatButton, RaisedButton }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class _MyHomePageState extends State<MyHomePage> {
+  String selectedWidget = 'FlatButton';
+  String selectedProperty = 'Elevation';
+  double configuredPadding = 8.0;
+
+  String getCode(double padding) {
+    return """
+FlatButton(
+  child: Text('Button label'),
+  elevation: $padding,
+),
+""";
   }
 
   @override
@@ -39,25 +46,96 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Text('Configuration:'),
+            ListTile(
+              title: const Text('Widget:'),
+              trailing: DropdownButton<String>(
+                value: selectedWidget,
+                onChanged: (String newValue) {
+                  setState(() {
+                    selectedWidget = newValue;
+                  });
+                },
+                items: <String>[
+                  'FlatButton',
+                  'RaisedButton',
+                  'Checkbox',
+                  'Radio',
+                  'Switch'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            ListTile(
+              title: const Text('Property:'),
+              trailing: DropdownButton<String>(
+                value: selectedProperty,
+                onChanged: (String newValue) {
+                  setState(() {
+                    selectedProperty = newValue;
+                  });
+                },
+                items: <String>[
+                  'Elevation',
+                  'BorderShape',
+                  'Color',
+                  'SplashColor'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+            ListTile(
+              title: Text('Property value: ${configuredPadding.toString()}'),
+              trailing: SizedBox(
+                width: 180.0,
+                child: Slider(
+                  value: configuredPadding,
+                  min: 0.0,
+                  max: 24.0,
+                  divisions: 3,
+                  onChanged: (double value) {
+                    setState(() {
+                      configuredPadding = value;
+                    });
+                  },
+                ),
+              ),
+            ),
+            Divider(),
+            Text('Output:'),
+            Center(
+              child: RaisedButton(
+                child: Text('Button label'),
+                onPressed: () {},
+                elevation: configuredPadding,
+              ),
+            ),
+            Divider(),
+            Text('Code:'),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                getCode(configuredPadding),
+                style: TextStyle(fontFamily: 'monospace'),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
